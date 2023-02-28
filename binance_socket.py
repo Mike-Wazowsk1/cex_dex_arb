3b940d4f197dc6113d65bb547358b75d64aa100e
Mike-Wazowsk1 <exectai@gmail.com>
2023-02-28 19:24:47 +0300

    add mean for ws

--- binance_socket.py
+++ binance_socket.py
@@ -1,9 +1,9 @@
 import asyncio
 from binance import AsyncClient, BinanceSocketManager
 from binance import ThreadedWebsocketManager
-import multiprocessing as mp 
+import multiprocessing as mp
 import threading as th
-from math import floor,ceil
+from math import floor, ceil
 import pickle as pkl
 import time
 # Importing libraries
@@ -11,6 +11,7 @@ from binance.client import Client
 import configparser
 from binance.streams import ThreadedWebsocketManager
 import requests
+import numpy as np
 
 # Loading keys from config file
 
@@ -18,7 +19,7 @@ import requests
 # client = Client(actual_api_key, actual_secret_key, tld="com")
 
 
-def printer(msg,path):
+def printer(msg, path):
     """
     Function to process the received messages
     param msg: input message
@@ -28,45 +29,59 @@ def printer(msg,path):
     timestamp = msg['lastUpdateId']
     asks = msg['asks']
     bids = msg['bids']
-    data = [symbol,timestamp,asks,bids]
-    # print(data)
-    with open(f"binance_data/{symbol}.pkl", 'wb') as f:
-        pkl.dump(data,f)
+
+    asks_price = np.array([float(x[0]) for x in asks[:15]])
+    asks_quantity = np.array([float(x[1]) for x in asks[:15]])
+    numerator = (asks_price * asks_quantity).sum()
+    denumerator = (asks_quantity).sum()
+    asks_avg_price = numerator/denumerator
+
+    bids_price = np.array([float(x[0]) for x in bids[:15]])
+    bids_quantity = np.array([float(x[1]) for x in bids[:15]])
+    numerator = (bids_price * bids_quantity).sum()
+    denumerator = (bids_quantity).sum()
+
+    bids_avg_price = numerator/denumerator
+
+    with open(f"binance_data/{symbol}_bids.pkl", 'wb') as f:
+        pkl.dump(bids_avg_price, f)
+    f.close()
+
+    with open(f"binance_data/{symbol}_asks.pkl", 'wb') as f:
+        pkl.dump(asks_avg_price, f)
     f.close()
 
 
-async def writer(bm,symbol,loop):
-        print(symbol)
-        bm.start_depth_socket(callback = printer, symbol=symbol, depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
-        time.sleep(0.1)
+async def writer(bm, symbol, loop):
+    print(symbol)
+    bm.start_depth_socket(callback=printer, symbol=symbol,
+                          depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
+    time.sleep(0.1)
+
+    # async with ts as tscm:
+    #     while True:
+    #         res = await tscm.recv()
+    #         timestamp = res['lastUpdateId']
+    #         asks = res['asks']
+    #         bids = res['bids']
+    #         data = [symbol,timestamp,asks,bids]
+    #         with open(f"binance_data/{symbol}.pkl", 'wb') as f:
+    #             pkl.dump(data,f)
+    #         f.close()
+    #         await asyncio.sleep(0.1)
 
 
-        # async with ts as tscm:
-        #     while True:
-        #         res = await tscm.recv()
-        #         timestamp = res['lastUpdateId']
-        #         asks = res['asks']
-        #         bids = res['bids']
-        #         data = [symbol,timestamp,asks,bids]
-        #         with open(f"binance_data/{symbol}.pkl", 'wb') as f:
-        #             pkl.dump(data,f)
-        #         f.close()
-        #         await asyncio.sleep(0.1)
-                
-
-
-def reciver(client,current_batch,global_dict):
+def reciver(client, current_batch, global_dict):
     twm = ThreadedWebsocketManager()
     twm.start()
     ss = []
     for symbol in current_batch:
-        twm.start_depth_socket(callback = printer, symbol=symbol, depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
+        twm.start_depth_socket(
+            callback=printer, symbol=symbol, depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
         ss.append(symbol)
     print(f"Len: {len(ss)} {ss[:3]}")
-        # time.sleep(3)
+    # time.sleep(3)
     twm.join()
-        
-
 
 
 # def proxy(client,current_batch,loop,type='process'):
@@ -81,25 +96,40 @@ async def get_snapshot(symbol):
     timestamp = r['lastUpdateId']
     asks = r['asks']
     bids = r['bids']
-    data = [symbol,timestamp,asks,bids]
-    # print(data)
-    with open(f"binance_data/{symbol.lower()}.pkl", 'wb') as f:
-        pkl.dump(data,f)
+    asks_price = np.array([float(x[0]) for x in asks[:15]])
+    asks_quantity = np.array([float(x[1]) for x in asks[:15]])
+    numerator = (asks_price * asks_quantity).sum()
+    denumerator = (asks_quantity).sum()
+    asks_avg_price = numerator/denumerator
+
+    bids_price = np.array([float(x[0]) for x in bids[:15]])
+    bids_quantity = np.array([float(x[1]) for x in bids[:15]])
+    numerator = (bids_price * bids_quantity).sum()
+    denumerator = (bids_quantity).sum()
+
+    bids_avg_price = numerator/denumerator
+
+    with open(f"binance_data/{symbol}_bids.pkl", 'wb') as f:
+        pkl.dump(bids_avg_price, f)
     f.close()
-        
+
+    with open(f"binance_data/{symbol}_asks.pkl", 'wb') as f:
+        pkl.dump(asks_avg_price, f)
+    f.close()
+
 
 
 async def main():
     global loop
     client = await AsyncClient.create()
-    
+
     info = await client.get_exchange_info()
     symbols = [x['symbol'] for x in info['symbols']]
     symbols = [x for x in set(symbols)]
     batch_size = ceil(1000)
     bm_count = ceil(len(symbols)/batch_size)
-    print(f"total pair: {len(symbols)} batch_size: {batch_size} bm_count: {bm_count} ")
-    # bm_count = 1
+    print(
+        f"total pair: {len(symbols)} batch_size: {batch_size} bm_count: {bm_count} ")
     for symbol in symbols:
         await get_snapshot(symbol)
 
@@ -108,7 +138,7 @@ async def main():
         current_batch = symbols[i*batch_size:batch_size*i+batch_size]
         # print(current_batch)
         print(current_batch[:3])
-        p = mp.Process(target = reciver, args=[client,current_batch,0])
+        p = mp.Process(target=reciver, args=[client, current_batch, 0])
         p.start()
         # p.join()
     # for p in ps:
