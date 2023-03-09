dee5ea17348ebed924e4c72a51fc3d90658115fa
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-10 00:57:05 +0300

    add print

--- sockets/binance_socket.py
+++ sockets/binance_socket.py
@@ -94,42 +94,42 @@ def reciver(client, current_batch, global_dict):
 #         asyncio.ensure_future(writer(client,current_batch,loop))
 
 def get_snapshot(symbol):
-    base_url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit=20'
-    msg = requests.get(base_url).json()
-    try:
-        timestamp = msg['lastUpdateId']
-    except:
-        timestamp = 9999
-    asks = msg['asks']
-    bids = msg['bids']
+    # base_url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit=20'
+    # msg = requests.get(base_url).json()
+    # try:
+    #     timestamp = msg['lastUpdateId']
+    # except:
+    #     timestamp = 9999
+    # asks = msg['asks']
+    # bids = msg['bids']
 
-    asks_price = np.array([float(x[0]) for x in asks[:15]])
-    asks_quantity = np.array([float(x[1]) for x in asks[:15]])
-    numerator = (asks_price * asks_quantity).sum()
-    asks_amount = (asks_quantity).sum()
+    # asks_price = np.array([float(x[0]) for x in asks[:15]])
+    # asks_quantity = np.array([float(x[1]) for x in asks[:15]])
+    # numerator = (asks_price * asks_quantity).sum()
+    # asks_amount = (asks_quantity).sum()
 
-    if asks_amount == 0:
-        asks_avg_price = 0
-        return 0
-    else:
-        asks_avg_price = numerator/asks_amount
+    # if asks_amount == 0:
+    #     asks_avg_price = 0
+    #     return 0
+    # else:
+    #     asks_avg_price = numerator/asks_amount
     
 
-    bids_price = np.array([float(x[0]) for x in bids[:15]])
-    bids_quantity = np.array([float(x[1]) for x in bids[:15]])
-    numerator = (bids_price * bids_quantity).sum()
-    bids_amount = (bids_quantity).sum()
+    # bids_price = np.array([float(x[0]) for x in bids[:15]])
+    # bids_quantity = np.array([float(x[1]) for x in bids[:15]])
+    # numerator = (bids_price * bids_quantity).sum()
+    # bids_amount = (bids_quantity).sum()
     
 
     
-    if bids_amount == 0:
-        bids_avg_price = 0
-        return 0
-    else:
-        bids_avg_price = numerator/bids_amount
+    # if bids_amount == 0:
+    #     bids_avg_price = 0
+    #     return 0
+    # else:
+    #     bids_avg_price = numerator/bids_amount
 
-    print("binance",symbol,asks_avg_price,bids_avg_price,asks_amount,bids_amount,int(timestamp))
-    db.init_snapshot(db_name="binance",symbol=symbol.lower(),asks_price=asks_avg_price,bids_price=bids_avg_price,asks_amount=asks_amount,bids_amount=bids_amount,timestamp=int(timestamp))
+    print("binance",symbol)#,asks_avg_price,bids_avg_price,asks_amount,bids_amount,int(timestamp))
+    db.init_snapshot(db_name="binance",symbol=symbol.lower(),asks_price=0,bids_price=0,asks_amount=0,bids_amount=0,timestamp=int(0))
 
 
     # with open(f"binance_data/{symbol}_bids.pkl", 'wb') as f:
