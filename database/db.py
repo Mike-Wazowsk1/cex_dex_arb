c69a366f5866b7b53fab95934a7a6e507015c06f
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-09 03:22:54 +0300

    upate db

--- database/db.py
+++ database/db.py
@@ -31,7 +31,6 @@ class DataBase:
     def init_snapshot(self, db_name, symbol, asks_price, bids_price, asks_amount, bids_amount, timestamp):
         q = f"INSERT INTO {db_name} (symbol,asks_price,bids_price, asks_amount,bids_amount,timestamp) VALUES ('{symbol}',{Decimal(asks_price)}, {Decimal(bids_price)},{Decimal(asks_amount)},{Decimal(bids_amount)}, {timestamp})"
         try:
-
             self.conn = psycopg2.connect(
                 host=DB.host,
                 database=DB.dbname,
@@ -46,25 +45,7 @@ class DataBase:
             self.conn.commit()
             self.cursor.close()
             self.conn.close()
-        except psycopg2.InterfaceError as exc:
-            print("I'm ProgrammingError")
-
-
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
-
-            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
-            self.conn.autocommit = True
-            self.cursor.execute(q)
-            self.conn.commit()
-            self.cursor.close()
-            self.conn.close()
-        except psycopg2.ProgrammingError as e:
-            print("I'm ProgrammingError")
-
-            print(e)
+        except:
             self.cursor.close()
             self.conn.close()
             self.conn = psycopg2.connect(host=DB.host,
@@ -76,51 +57,27 @@ class DataBase:
             self.conn.autocommit = True
             self.cursor.execute(q)
             self.conn.commit()
-        except psycopg2.OperationalError as e:
-            print("I'm operation")
-            print(e)
-            self.cursor.close()
-            self.conn.close()
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
-
-            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
-            self.conn.autocommit = True
-            self.cursor.execute(q)
-            self.conn.commit()
-        finally:
-            print("I'm finally")
-            self.cursor.close()
-            self.conn.close()
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
-
-            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
-            self.conn.autocommit = True
-            self.cursor.execute(q)
-            self.conn.commit()
-
 
     def update_db(self, db_name, symbol, asks_price, bids_price, asks_amount, bids_amount, timestamp):
         q = f"UPDATE {db_name} SET asks_price = {asks_price},bids_price = {Decimal(bids_price)},asks_amount = {Decimal(asks_amount)},bids_amount = {Decimal(bids_amount)}, timestamp = {timestamp} WHERE symbol = '{symbol}'"
-        if self.conn.closed:
+        try:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
 
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
+            )
 
             self.cursor = self.conn.cursor(cursor_factory=DictCursor)
             self.conn.autocommit = True
-        try:
             self.cursor.execute(q)
             self.conn.commit()
-        except psycopg2.InterfaceError as exc:
-
+            self.cursor.close()
+            self.conn.close()
+        except:
+            self.cursor.close()
+            self.conn.close()
             self.conn = psycopg2.connect(host=DB.host,
                                          database=DB.dbname,
                                          user=DB.user,
@@ -134,59 +91,108 @@ class DataBase:
     def get_from_db(self, db_name, symbol):
         q = f"SELECT asks_price,bids_price, asks_amount,bids_amount,timestamp from {db_name} WHERE symbol = '{symbol}' ORDER BY symbol"
         try:
-            self.cursor.execute(q)
-            data = self.cursor.fetchall()
-            return data
-        except psycopg2.InterfaceError as exc:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
 
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password,
-                                         )
+            )
 
             self.cursor = self.conn.cursor(cursor_factory=DictCursor)
             self.conn.autocommit = True
             self.cursor.execute(q)
+
             data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
+            return data
+        except:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
+
+            )
+
+            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+            self.conn.autocommit = True
+            self.cursor.execute(q)
+
+            data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
             return data
 
     def get_arb_info(self, db_name):
         q = f"SELECT symbol,asks_price,bids_price, asks_amount,bids_amount,timestamp from {db_name} ORDER BY symbol"
         try:
-            self.cursor.execute(q)
-            data = self.cursor.fetchall()
-            return data
-        except psycopg2.InterfaceError as exc:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
 
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
+            )
 
             self.cursor = self.conn.cursor(cursor_factory=DictCursor)
             self.conn.autocommit = True
             self.cursor.execute(q)
             data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
+            return data
+        except:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
+
+            )
+
+            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+            self.conn.autocommit = True
+            self.cursor.execute(q)
+            data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
             return data
 
     def get_symbols_data(self, db_name, symbols):
         q = f"""SELECT symbol,asks_price,bids_price, asks_amount,bids_amount,timestamp from {db_name} WHERE symbol in ({str(symbols)[1:-1]}) ORDER BY symbol"""
         try:
-            self.cursor.execute(q)
-            data = self.cursor.fetchall()
-            return data
-        except psycopg2.InterfaceError as exc:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
 
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
+            )
 
             self.cursor = self.conn.cursor(cursor_factory=DictCursor)
             self.conn.autocommit = True
             self.cursor.execute(q)
             data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
+            return data
+        except:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
+
+            )
+
+            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+            self.conn.autocommit = True
+            self.cursor.execute(q)
+            data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
             return data
 
     def get_all_tables(self):
@@ -195,18 +201,34 @@ SELECT table_name FROM information_schema.tables
 WHERE table_schema = 'public' and table_catalog='cex_dex'
 """
         try:
-            self.cursor.execute(q)
-            data = self.cursor.fetchall()
-            return data
-        except psycopg2.InterfaceError as exc:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
 
-            self.conn = psycopg2.connect(host=DB.host,
-                                         database=DB.dbname,
-                                         user=DB.user,
-                                         password=DB.password, )
+            )
 
             self.cursor = self.conn.cursor(cursor_factory=DictCursor)
             self.conn.autocommit = True
             self.cursor.execute(q)
             data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
+            return data
+        except:
+            self.conn = psycopg2.connect(
+                host=DB.host,
+                database=DB.dbname,
+                user=DB.user,
+                password=DB.password,
+
+            )
+
+            self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+            self.conn.autocommit = True
+            self.cursor.execute(q)
+            data = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
             return data
