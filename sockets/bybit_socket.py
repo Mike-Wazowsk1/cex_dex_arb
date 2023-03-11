1f5282678d8d77c87b6ff4f25da21f7a5a7beb26
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-11 05:13:56 +0300

    add count for orders

--- sockets/bybit_socket.py
+++ sockets/bybit_socket.py
@@ -27,10 +27,9 @@ def handle_orderbook(message):
         prev_quantity = 0
         for i, val in enumerate(asks_quantity):
             if quantity < user_max_amount:
-                quantity += asks_price[i] * val if quantity + asks_price[i] * \
-                    val <= user_max_amount else asks_price[i] * (user_max_amount - prev_quantity)
-                mean_price += asks_price[i] * (asks_price[i] * val) if quantity < user_max_amount else (
-                    asks_price[i] * asks_price[i] * (user_max_amount - prev_quantity))
+                quantity += val if quantity + val <= user_max_amount else (user_max_amount - prev_quantity)
+                mean_price += (asks_price[i] * val) if quantity <= user_max_amount else (
+                    asks_price[i] * (user_max_amount - prev_quantity))
                 prev_quantity = quantity
                 count += 1
 
@@ -46,10 +45,9 @@ def handle_orderbook(message):
         prev_quantity = 0
         for i, val in enumerate(bids_quantity):
             if quantity < user_max_amount:
-                quantity += bids_price[i] * val if quantity + bids_price[i] * \
-                    val <= user_max_amount else bids_price[i] * (user_max_amount - prev_quantity)
-                mean_price += bids_price[i] * (bids_price[i] * val) if quantity < user_max_amount else (
-                    bids_price[i] * bids_price[i] * (user_max_amount - prev_quantity))
+                quantity += val if quantity + val <= user_max_amount else (user_max_amount - prev_quantity)
+                mean_price += (bids_price[i] * val) if quantity <= user_max_amount else (
+                    bids_price[i] * (user_max_amount - prev_quantity))
                 prev_quantity = quantity
                 count += 1
         bids_amount = min(quantity, user_max_amount)
@@ -76,10 +74,9 @@ def handle_orderbook(message):
 
         for i, val in enumerate(asks_quantity):
             if quantity < user_max_amount:
-                quantity += asks_price[i] * val if quantity + asks_price[i] * \
-                    val <= user_max_amount else asks_price[i] * (user_max_amount - prev_quantity)
-                mean_price += asks_price[i] * (asks_price[i] * val) if quantity < user_max_amount else (
-                    asks_price[i] * asks_price[i] * (user_max_amount - prev_quantity))
+                quantity += val if quantity + val <= user_max_amount else (user_max_amount - prev_quantity)
+                mean_price += (asks_price[i] * val) if quantity <= user_max_amount else (
+                    asks_price[i] * (user_max_amount - prev_quantity))
                 prev_quantity = quantity
                 count += 1
 
@@ -95,10 +92,9 @@ def handle_orderbook(message):
         prev_quantity = 0
         for i, val in enumerate(bids_quantity):
             if quantity < user_max_amount:
-                quantity += bids_price[i] * val if quantity + bids_price[i] * \
-                    val <= user_max_amount else bids_price[i] * (user_max_amount - prev_quantity)
-                mean_price += bids_price[i] * (bids_price[i] * val) if quantity < user_max_amount else (
-                    bids_price[i] * bids_price[i] * (user_max_amount - prev_quantity))
+                quantity += val if quantity + val <= user_max_amount else (user_max_amount - prev_quantity)
+                mean_price += (bids_price[i] * val) if quantity <= user_max_amount else (
+                    bids_price[i] * (user_max_amount - prev_quantity))
                 prev_quantity = quantity
                 count += 1
 
