ce8c293c025fc6d0976f8d202ec212213f7637f8
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-11 12:21:35 +0300

    add count for orders

--- sockets/binance_socket.py
+++ sockets/binance_socket.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+import logging
 from binance import AsyncClient, BinanceSocketManager
 from binance import ThreadedWebsocketManager
 import multiprocessing as mp
@@ -10,12 +12,135 @@ import time
 from binance.client import Client
 import configparser
 from binance.streams import ThreadedWebsocketManager
+from pandas import Timestamp
 import requests
 import numpy as np
 from database.db import DataBase
+from binance.client import Client
 
 
 db = DataBase()
+order_book = {
+    "lastUpdateId": 0,
+    "bids": [],
+    "asks": []
+}
+base_url = 'https://api.binance.com'
+stream_url = 'wss://stream.binance.com:9443/ws'
+
+client = Client()
+
+info = client.get_exchange_info()
+symbols = [x['symbol'] for x in info['symbols']]
+symbols = [x for x in set(symbols)]
+
+
+def get_snapshot(symbol):
+    base_url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit=1000'
+    msg = requests.get(base_url).json()
+    return msg
+
+
+def manage_order_book(side, update):
+    """
+    Updates local order book's bid or ask lists based on the received update ([price, quantity])
+    """
+    global order_book
+    price, quantity = update
+    # price exists: remove or update local order
+    for i in range(0, len(order_book[side])):
+        if price == order_book[side][i][0]:
+            # quantity is 0: remove
+            if float(quantity) == 0:
+                order_book[side].pop(i)
+                return
+            else:
+                # quantity is not 0: update the order with new quantity
+                order_book[side][i] = update
+                return
+
+    # price not found: add new order
+    if float(quantity) != 0:
+        order_book[side].insert(-1, update)
+        if side == 'asks':
+            # asks prices in ascendant order
+            order_book[side] = sorted(
+                order_book[side], key=lambda x: float(x[0]))
+        else:
+            order_book[side] = sorted(order_book[side], key=lambda x: float(
+                x[0]), reverse=True)  # bids prices in descendant order
+
+    if len(order_book[side]) > 1000:
+        order_book[side].pop(len(order_book[side])-1)
+
+
+def process_updates(message):
+
+    for update in message['b']:
+        manage_order_book('bids', update)
+    for update in message['a']:
+        manage_order_book('asks', update)
+
+
+def to_db(asks, bids, symbol):
+    timestamp = time.time()
+    asks_price = np.array([float(x[0]) for x in asks[:15]])
+    asks_quantity = np.array([float(x[1]) for x in asks[:15]])
+    user_max_amount = float(db.get_info_col('max_amount'))
+    quantity = 0
+    count = 0
+    mean_price = 0
+    usdt_quantity = 0
+    for i, val in enumerate(asks_quantity):
+        if usdt_quantity < user_max_amount:
+            quantity += val
+            mean_price += asks_price[i] * val
+            usdt_quantity += quantity * asks_price[i]
+            count += 1
+
+    asks_amount = quantity
+    asks_avg_price = mean_price/asks_amount
+
+    bids_price = np.array([float(x[0]) for x in bids[:15]])
+    bids_quantity = np.array([float(x[1]) for x in bids[:15]])
+
+    quantity = 0
+    count = 0
+    mean_price = 0
+    usdt_quantity = 0
+    for i, val in enumerate(bids_quantity):
+        if usdt_quantity < user_max_amount:
+            quantity += val
+            mean_price += bids_price[i] * val
+            usdt_quantity += quantity * bids_price[i]
+            count += 1
+
+    bids_amount = quantity
+    bids_avg_price = mean_price/bids_amount
+
+    db.update_db(db_name="binance", symbol=symbol.lower(), asks_price=asks_avg_price,
+                 bids_price=bids_avg_price, asks_amount=asks_amount, bids_amount=bids_amount, count=count, timestamp=int(timestamp))
+
+
+def message_handler(message, path):
+    global order_book, symbols
+    symbol = path.split("@")[0]
+
+    if "depthUpdate" in json.dumps(message):
+        last_update_id = order_book['lastUpdateId']
+        if message['u'] <= last_update_id:
+            return
+        if message['U'] <= last_update_id + 1 <= message['u']:
+            order_book['lastUpdateId'] = message['u']
+            process_updates(message)
+        else:
+            logging.info('Out of sync, re-syncing...')
+            for symbol in symbols:
+                order_book = get_snapshot(symbol)
+
+        bids = np.array(order_book['bids'][:, 0])[:15]
+        asks = np.array(order_book['asks'][:, 0])[:15]
+        to_db(asks, bids, symbol)
 
 
 def printer(msg, path):
@@ -74,7 +199,7 @@ def printer(msg, path):
 
 async def writer(bm, symbol, loop):
     print(symbol)
-    bm.start_depth_socket(callback=printer, symbol=symbol,
+    bm.start_depth_socket(callback=message_handler, symbol=symbol,
                           depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
 
 
@@ -84,13 +209,13 @@ def reciver(client, current_batch, global_dict):
     ss = []
     for symbol in current_batch:
         twm.start_depth_socket(
-            callback=printer, symbol=symbol, depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
+            callback=message_handler, symbol=symbol, depth=BinanceSocketManager.WEBSOCKET_DEPTH_20)
         ss.append(symbol)
     print(f"Len: {len(ss)} {ss[:3]}")
     twm.join()
 
 
-def get_snapshot(symbol):
+def get_init(symbol):
     # base_url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit=20'
     # msg = requests.get(base_url).json()
     # try:
@@ -130,17 +255,12 @@ def get_snapshot(symbol):
 
 async def main():
     global loop
-    client = await AsyncClient.create()
-
-    info = await client.get_exchange_info()
-    symbols = [x['symbol'] for x in info['symbols']]
-    symbols = [x for x in set(symbols)]
     batch_size = ceil(300)
     bm_count = ceil(len(symbols)/batch_size)
     print(
         f"total pair: {len(symbols)} batch_size: {batch_size} bm_count: {bm_count} ")
     with mp.Pool() as pool:
-        pool.map(get_snapshot, symbols)
+        pool.map(get_init, symbols)
     for i in range(bm_count):
         current_batch = symbols[i*batch_size:batch_size*i+batch_size]
         p = mp.Process(target=reciver, args=[client, current_batch, 0])
