38e499a2f1f22b399112763a1b88b741bff7f2e1
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-13 01:40:00 +0300

    add async

--- sockets/binance_socket.py
+++ sockets/binance_socket.py
@@ -74,7 +74,7 @@ def init_snapshot(symbol):
         """
         Retrieve order book
         """
-        time.sleep(random.randint(10,20))
+        time.sleep(60)
         base_url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit=1000'
         msg = requests.get(base_url).json()
         CNT += 1
