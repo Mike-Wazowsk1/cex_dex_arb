f43297963012dcc772aa6a174da12dfedac96af5
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-13 06:54:37 +0300

    bybit print ltcusdt

--- sockets/bybit_socket.py
+++ sockets/bybit_socket.py
@@ -96,13 +96,14 @@ def handle_orderbook(message):
 
         bids_amount = quantity
         bids_avg_price = mean_price/bids_amount
-        if symbol.lower() == 'ltcusdt':
-            print(asks_price)
-            print(bids_price)
-            
+
 
         db.update_db(db_name="bybit", symbol=symbol.lower(), asks_price=asks_avg_price,
                      bids_price=bids_avg_price, asks_amount=asks_amount, bids_amount=bids_amount, count=count, timestamp=int(timestamp))
+    if symbol.lower() == 'ltcusdt':
+        print(asks_price)
+        print(bids_price)
+        
 
 
 # Similarly, if you want to listen to the WebSockets of other markets:
