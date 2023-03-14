094d93c080427146838072c71d1ae82fae86cf99
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 16:41:59 +0300

    add cython ext for db

--- database/db_c.pyx
+++ database/db_c.pyx
@@ -4,12 +4,12 @@ from config import config
 DB = config.DB
 
 
-class DataBase:
+cdef class DataBase:
     def __init__(self):
         pass
 
-    cdef init_snapshot(self, db_name, symbol, asks_price, bids_price, asks_amount, bids_amount, count, timestamp):
-        q = f"INSERT INTO {db_name} (symbol,asks_price,bids_price, asks_amount,bids_amount,count,timestamp) VALUES ('{symbol}',{asks_price}, {bids_price},{asks_amount},{bids_amount},{count}, {timestamp})"
+    cpdef init_snapshot(self, db_name, symbol, asks_price, bids_price, asks_amount, bids_amount, count, timestamp):
+        cdef q = f"INSERT INTO {db_name} (symbol,asks_price,bids_price, asks_amount,bids_amount,count,timestamp) VALUES ('{symbol}',{asks_price}, {bids_price},{asks_amount},{bids_amount},{count}, {timestamp})"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -38,8 +38,8 @@ class DataBase:
             self.cursor.execute(q)
             self.conn.commit()
 
-    cdef update_db(self, db_name, symbol, asks_price, bids_price, asks_amount, bids_amount, count, timestamp):
-        q = f"UPDATE {db_name} SET asks_price = {asks_price},bids_price = {bids_price},asks_amount = {asks_amount},bids_amount = {bids_amount}, timestamp = {timestamp},count={count} WHERE symbol = '{symbol}'"
+    cpdef update_db(self, db_name, symbol, asks_price, bids_price, asks_amount, bids_amount, count, timestamp):
+        cdef q = f"UPDATE {db_name} SET asks_price = {asks_price},bids_price = {bids_price},asks_amount = {asks_amount},bids_amount = {bids_amount}, timestamp = {timestamp},count={count} WHERE symbol = '{symbol}'"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -68,8 +68,8 @@ class DataBase:
             self.cursor.execute(q)
             self.conn.commit()
 
-    cdef get_from_db(self, db_name, symbol):
-        q = f"SELECT asks_price,bids_price, asks_amount,bids_amount,count,timestamp from {db_name} WHERE symbol = '{symbol}' ORDER BY symbol"
+    cpdef get_from_db(self, db_name, symbol):
+        cdef q = f"SELECT asks_price,bids_price, asks_amount,bids_amount,count,timestamp from {db_name} WHERE symbol = '{symbol}' ORDER BY symbol"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -105,8 +105,8 @@ class DataBase:
             self.conn.close()
             return data
 
-    cdef get_arb_info(self, db_name):
-        q = f"SELECT symbol,asks_price,bids_price, asks_amount,bids_amount,count,timestamp from {db_name} ORDER BY symbol"
+    cpdef get_arb_info(self, db_name):
+        cdef q = f"SELECT symbol,asks_price,bids_price, asks_amount,bids_amount,count,timestamp from {db_name} ORDER BY symbol"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -140,8 +140,8 @@ class DataBase:
             self.conn.close()
             return data
 
-    cdef get_symbols_data(self, db_name, symbols):
-        q = f"""SELECT symbol,asks_price,bids_price, asks_amount,bids_amount,count,timestamp from {db_name} WHERE symbol in ({str(symbols)[1:-1]}) ORDER BY symbol"""
+    cpdef get_symbols_data(self, db_name, symbols):
+        cdef q = f"""SELECT symbol,asks_price,bids_price, asks_amount,bids_amount,count,timestamp from {db_name} WHERE symbol in ({str(symbols)[1:-1]}) ORDER BY symbol"""
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -175,8 +175,8 @@ class DataBase:
             self.conn.close()
             return data
 
-    cdef get_all_tables(self):
-        q = """
+    cpdef get_all_tables(self):
+        cdef q = """
 SELECT table_name FROM information_schema.tables 
 WHERE table_schema = 'public' and table_catalog='cex_dex' and table_name != 'info'
 """
@@ -213,8 +213,8 @@ WHERE table_schema = 'public' and table_catalog='cex_dex' and table_name != 'inf
             self.conn.close()
             return data
 
-    cdef del_from_db(self, db_name, symbol):
-        q = f"DELETE FROM {db_name} WHERE symbol = '{symbol}'"
+    cpdef del_from_db(self, db_name, symbol):
+        cdef q = f"DELETE FROM {db_name} WHERE symbol = '{symbol}'"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -243,8 +243,8 @@ WHERE table_schema = 'public' and table_catalog='cex_dex' and table_name != 'inf
             self.cursor.execute(q)
             self.conn.commit()
 
-    cdef get_info(self):
-        q = f"""SELECT min_profit,min_amount,max_amount FROM info"""
+    cpdef get_info(self):
+        cdef q = f"""SELECT min_profit,min_amount,max_amount FROM info"""
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -278,8 +278,8 @@ WHERE table_schema = 'public' and table_catalog='cex_dex' and table_name != 'inf
             self.conn.close()
             return data
 
-    cdef get_info_col(self, col):
-        q = f"""SELECT {col} FROM info"""
+    cpdef get_info_col(self, col):
+        cdef q = f"""SELECT {col} FROM info"""
         self.conn = psycopg2.connect(
             host=DB.host,
             database=DB.dbname,
@@ -295,8 +295,8 @@ WHERE table_schema = 'public' and table_catalog='cex_dex' and table_name != 'inf
         return data[0][0]
   
 
-    cdef update_info(self, col, val):
-        q = f"UPDATE info SET {col} = {val}"
+    cpdef update_info(self, col, val):
+        cdef q = f"UPDATE info SET {col} = {val}"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
@@ -327,8 +327,8 @@ WHERE table_schema = 'public' and table_catalog='cex_dex' and table_name != 'inf
             self.cursor.close()
             self.conn.close()
 
-    cdef get_command(self, symbol, db_name):
-        q = f"select symbol, asks_price as ask,bids_price as bid,timestamp,count from {db_name} where timestamp!=0 and symbol = '{symbol}' order by symbol"
+    cpdef get_command(self, symbol, db_name):
+        cdef q = f"select symbol, asks_price as ask,bids_price as bid,timestamp,count from {db_name} where timestamp!=0 and symbol = '{symbol}' order by symbol"
         try:
             self.conn = psycopg2.connect(
                 host=DB.host,
