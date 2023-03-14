3024246022360e47f284232517503957098ff85a
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 14:56:33 +0300

    most optimization cython

--- sockets/binance_builder.py
+++ sockets/binance_builder.py
@@ -6,4 +6,5 @@ from Cython.Distutils import build_ext
 setup(name = 'binance_socket_c',ext_modules=[Extension("binance_socket_c",
                                                         sources=['binance_socket_c.pyx'],
                                                         extra_compile_args=['-O3'],
-                                                        )],cmdclass = {'build_ext': build_ext})
+                                                        )],cmdclass = {'build_ext': build_ext},
+                                                        language_level='3')
