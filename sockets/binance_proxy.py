37032eb847e246dc7816cd4485556b523eb122d5
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 15:31:02 +0300

    wtf

--- sockets/binance_proxy.py
+++ sockets/binance_proxy.py
@@ -1,5 +1,5 @@
 #type: ignore
-from cex_dex_arb.sockets.binance_socket_c import main
+from binance_socket_c import main
 import asyncio
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
