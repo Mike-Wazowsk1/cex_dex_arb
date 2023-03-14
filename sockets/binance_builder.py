0ae02a2d26354e7d5be4f3a1bc494f48df703f80
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 15:25:37 +0300

    add language level

--- sockets/binance_builder.py
+++ sockets/binance_builder.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-ext = [Extension("binance_socket_ci",
-                 sources=['binance_socket_ci.pyx'],
+ext = [Extension("binance_socket_c",
+                 sources=['binance_socket_c.pyx'],
                  )]
 for e in ext:
     e.cython_directives = {'language_level': "3"} #all are Python-3
-setup(name='binance_socket_ci', ext_modules=ext, cmdclass={
-      'build_ext': build_ext}, language_level=3)
+setup(name='binance_socket_c', ext_modules=ext, cmdclass={
+      'build_ext': build_ext})
--- sockets/binance_proxy.py
+++ sockets/binance_proxy.py
@@ -1,5 +1,5 @@
 #type: ignore
-from cex_dex_arb.sockets.binance_socket_ci import main
+from cex_dex_arb.sockets.binance_socket_c import main
 import asyncio
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
