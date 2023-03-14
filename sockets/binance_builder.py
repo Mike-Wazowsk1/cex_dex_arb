0d1924494fc3ec1b130c61298ca33e09c427b4c7
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 14:48:29 +0300

    most optimization cython

--- sockets/binance_builder.py
+++ sockets/binance_builder.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 
 setup(name = 'binance_socket_c',ext_modules=[Extension("binance_socket_c",
-                                                        sources=['binance_socket_c.px'],
+                                                        sources=['binance_socket_c.pyx'],
                                                         extra_compile_args=['/Ox'])])
