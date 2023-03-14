dc6e699bdb5e84d73a5e6186663f902445e49b9a
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 15:20:29 +0300

    add language level

--- sockets/binance_builder.py
+++ sockets/binance_builder.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-
-setup(name = 'binance_socket_c',ext_modules=[Extension("binance_socket_c",
-                                                        sources=['binance_socket_c.pyx'],
-                                                        )],cmdclass = {'build_ext': build_ext},language_level=3)
+ext = [Extension("binance_socket_c",
+                 sources=['binance_socket_c.pyx'],
+                 )]
+for e in ext:
+    e.cython_directives = {'language_level': "3"} #all are Python-3
+setup(name='binance_socket_c', ext_modules=ext, cmdclass={
+      'build_ext': build_ext}, language_level=3)
