264c3ff4df6c1917aaaa554bbad4898937201395
Mike-Wazowsk1 <exectai@gmail.com>
2023-03-14 14:36:32 +0300

    only 3 symbols

--- sockets/binance_builder.py
+++ sockets/binance_builder.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize('binance_socket_c.pyx',language_level=3),language_level=3,extra_compile_args=["-O3"] )
+setup(ext_modules = cythonize('binance_socket_c.pyx',language_level=3,extra_compile_args=["-O3"] ))
